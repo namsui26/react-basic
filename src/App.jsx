ba95cf2ac20f1f6afcbbf306d9d4a0021fd9f127
namsui26 <161566381+namsui26@users.noreply.github.com>
2024-05-29 17:12:07 +0900

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
-import { Fragment } from "react";
 import { useState } from "react";
-import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const plusCount = () => {
-    setCount(count +1);
+	const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+	
+	const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
-  const minusCount = () => {
-    setCount(count -1);
-  };
-  const reset = () => {
-    setCount(0);
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
   return (
-    <Fragment>
-      <button className="minusCountButton" onClick={minusCount}>-</button>
-      Count: {count}
-      <button className="plusCountButton" onClick={plusCount}>+</button>
-      <button className="resetButton" onClick={reset}>reset</button>
-    </Fragment>
+    <form>
+      <div>
+        <label htmlFor="email">이메일:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">비밀번호:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="passwordConfirm">비밀번호 확인:</label>
+        <input
+          type="password"
+          id="passwordConfirm"
+        />
+      </div>
+      <button type="submit">회원가입</button>
+    </form>
   );
 }
 
